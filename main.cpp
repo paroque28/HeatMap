590833e448598cff105e51bb02543c3296975607
Pablo Rodriguez <paroque28@gmail.com>
2017-05-25 16:32:50 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+// Copyright Vladimir Prus 2002-2004.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+/* The simplest usage of the library.
+ */
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main(int ac, const char* av[])
+{
+    try {
+
+        po::options_description desc("Allowed options");
+        desc.add_options()
+                ("help", "produce help message")
+                ("size", po::value<double>(), "set matrix size")
+                ;
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(ac, av, desc), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            cout << desc << "\n";
+            return 0;
+        }
+
+        if (vm.count("size")) {
+            cout << "Matrix size was set to "
+                 << vm["size"].as<double>() << ".\n";
+        } else {
+            cout << "Matrix size was not set.\n";
+        }
+    }
+    catch(exception& e) {
+        cerr << "error: " << e.what() << "\n";
+        return 1;
+    }
+    catch(...) {
+        cerr << "Exception of unknown type!\n";
+    }
+
+    return 0;
+}
---
+++ src/Liebmann.h
@@ -0,0 +1,8 @@
+//
+// Created by pablorod on 5/25/2017.
+//
+
+#ifndef HEATMAP_LIEBMANN_H
+#define HEATMAP_LIEBMANN_H
+
+#endif //HEATMAP_LIEBMANN_H
