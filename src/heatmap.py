1456bda2ed5314ca538f99f261eef2c107637b96
ferllini13 <ferllini_13@hotmail.com>
2017-05-31 12:29:49 -0600

    coneccion c++,python y cambios grafico

--- src/heatmap.py
+++ src/heatmap.py
@@ -7,7 +7,7 @@ def grafico(n,x,y,u,v,mat):
     mat.reverse()
     mat=np.array(mat)
 
-    plt.imshow(mat, interpolation='sinc', extent=[0,n,0,n], vmin=0.0)
+    plt.imshow(mat, interpolation='sinc', extent=[0,n-1,0,n-1], vmin=0.0)
     plt.colorbar().set_label('Temperatura')
     if x==[] or y==[] or u==[] or v==[]: 
         plt.quiver(x, y, u, v, alpha=.5)
