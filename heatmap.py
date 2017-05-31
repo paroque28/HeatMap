19714ee32b148445b26dd76ccbbefb92304c17a8
ferllini13 <ferllini_13@hotmail.com>
2017-05-30 19:03:26 -0600

    grafica de vectores

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def grafico(n,mat):
+def grafico(n,x,y,u,v,mat):
 
 
     mat.reverse()
@@ -9,5 +9,10 @@ def grafico(n,mat):
 
     plt.imshow(mat, interpolation='sinc', extent=[0,n,0,n], vmin=0.0)
     plt.colorbar().set_label('Temperatura')
+    if x==[] or y==[] or u==[] or v==[]: 
+        plt.quiver(x, y, u, v, alpha=.5)
+        plt.quiver(x, y, u, v, edgecolor='k', facecolor='None', linewidth=.5)
+
+        
     plt.show()
 
